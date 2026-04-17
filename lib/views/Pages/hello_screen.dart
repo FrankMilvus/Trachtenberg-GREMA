ec76dbb1be9429007e316bd4eddb3169d472945b
FrankMilvus <frankmilvus@gmail.com>
2026-04-17 23:39:12 +0300

    Refactor HelloScreen to StatefulWidget; add toggle for description visibility

--- lib/views/Pages/hello_screen.dart
+++ lib/views/Pages/hello_screen.dart
@@ -3,15 +3,42 @@ import 'package:trachtenberg_grema/l10n/app_localizations.dart';
 
 import '../../constants/text_styles.dart';
 
-class HelloScreen extends StatelessWidget {
+class HelloScreen extends StatefulWidget {
   const HelloScreen({super.key});
+
+  @override
+  State<HelloScreen> createState() => _HelloScreenState();
+}
+
+class _HelloScreenState extends State<HelloScreen> {
+  bool _showDescription = false;
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Center(child: Text(l10n.hello, style: KTextStyle.body(context))),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(l10n.hello, style: KTextStyle.body(context)),
+            const SizedBox(height: 8),
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  _showDescription = !_showDescription;
+                });
+              },
+              child: Text(_showDescription ? "read less" : "read more"),
+            ),
+            if (_showDescription) ...[
+              const SizedBox(height: 8),
+              Text('detailed description', style: KTextStyle.body(context)),
+            ],
+          ],
+        ),
+      ),
     );
   }
 }
